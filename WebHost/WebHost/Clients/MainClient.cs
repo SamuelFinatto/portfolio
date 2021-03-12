47e67163cc3d187b58a70b82ddffb2a9166318c1
Samuel Finatto <samuel.finatto@andritz.com>
2021-03-11 22:37:29 -0300

    Implemented Initial ASP.NET Web Hosting
    Signed-off-by: Samuel Finatto <samuel.finatto@andritz.com>

---
+++ WebHost/WebHost/Clients/MainClient.cs
@@ -0,0 +1,57 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using WebHost.Data;
+
+namespace WebHost.Clients
+{
+    public class MainClient
+    {
+        public HttpClient Client { get; }
+
+        public MainClient(HttpClient client)
+        {
+            client.BaseAddress = new Uri("http://localhost:3000/");
+            client.DefaultRequestHeaders.Add("Accept",
+            "application/json");
+
+            Client = client;
+        }
+
+        public async Task<string> OnGet()
+        {
+            var response = await Client.GetAsync("/");
+
+            if (response.IsSuccessStatusCode)
+            {
+                var responseStream = await response.Content.ReadAsStringAsync();
+                //var result = await JsonSerializer.DeserializeAsync<string>(responseStream);
+                return responseStream;
+            }
+            else
+            {
+                return "ERROR";
+            }
+
+        }
+
+        public async ValueTask<List<WeatherForecast>> GetWeatherForecastsAsync()
+        {
+            var response = await Client.GetAsync("/weather/");
+
+            if(response.IsSuccessStatusCode)
+            {
+                var responseStream = await response.Content.ReadAsStringAsync();
+                return JsonConvert.DeserializeObject<IEnumerable<WeatherForecast>>(responseStream).ToList();
+            }
+            else
+            {
+                return new List<WeatherForecast>();
+            }
+        }
+    }
+}
